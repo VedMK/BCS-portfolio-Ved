2fff0ab71c579adaa9c0a5c6d6b2e5e6c745fba8
VedMK <vedmkapadia@gmail.com>
2026-09-16 12:46:46 +0300

    Update Directory fuzzer.py

--- projects/directory-fuzzer/Directory fuzzer.py
+++ projects/directory-fuzzer/Directory fuzzer.py
@@ -2,22 +2,15 @@ import requests as req # requests library is a requirement, ensure you have it i
 
 target=input("Insert domain name/ip-address here")
 dir_file="directories.txt"
-def chk_url(url): # Checking response headers
 
-    try:
-        response = req.get(url, timeout=5)
-        output = {
-            "url": url,
-            "SC": response.status_code,
-            "Clen": len(response.content)
+def chk_url(url): # Checking response headers
+    response=req.get(url,timeout=5)
+    output={
+        "url":url,
+        "Status-Code":response.status_code,
+        "Content-length":len(response.content)
         }
-        return output
-    except req.exceptions.Timeout:
-        print(f"Timedout at: {url}")
-        return None
-    except req.exceptions.RequestException as error:
-        print(f"Error at: {url} -> {error}")
-        return None
+    return output
 
 with open(dir_file,"r") as f: # Accessing directory wordlist,calling the check function and displaying the headers for user to analyze
     for directory in f.readlines():
